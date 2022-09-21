bd9889feff61a4e450f44e11ee50bcd17f17b2f5
aayar94 <a.ayar94@gmail.com>
2022-09-21 23:51:05 +0300

    Ders_2_OOP_1

--- src/Gun2_Ders_OOP_1/Main.java
+++ src/Gun2_Ders_OOP_1/Main.java
@@ -49,7 +49,7 @@ public class Main {
         corporateCustomer.setCompanyName("Java");
         corporateCustomer.setTaxNumber("1234567890");
 
-        Customer[] customers = {IndividualCustomer,CorporateCustomer};
+        Customer[] customers = {individualCustomer,corporateCustomer};
 
     }
 
