f11d22118f89d1b9616c906cac2d3f20ff65a6f5
aayar94 <a.ayar94@gmail.com>
2022-09-21 23:48:12 +0300

    Ders_2_OOP_1

--- src/Gun2_Ders_OOP_1/Main.java
+++ src/Gun2_Ders_OOP_1/Main.java
@@ -2,23 +2,55 @@ package Gun2_Ders_OOP_1;
 
 public class Main {
     public static void main(String[] args){
-        String mesaj = "Vade oranı";
 
         Product product1 = new Product();
-        // set value
-        product1.name="Delongi Kahve Makinesi";
-        product1.unitPrice=7500;
-        product1.discount=7;
-        product1.unitInStock=3;
-        product1.imageUrl="bilmemne.jpg";
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setDiscount(7);
+        product1.setUnitPrice(7500);
+        product1.setImageUrl("image1.jpg");
+        product1.setUnitInStock(3);
 
-        //get
-        System.out.println(product1.name);
+        Product product2= new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setDiscount(3);
+        product2.setUnitPrice(4500);
+        product2.setImageUrl("image2.jpg");
+        product2.setUnitInStock(2);
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setDiscount(5);
+        product3.setUnitPrice(6500);
+        product3.setImageUrl("image3.jpg");
+        product3.setUnitInStock(5);
+
+        Product[] products = {product1,product2,product3};
+
+        System.out.println("<ul>");
+
+        for (Product product: products) {
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
 
 
 
+        IndividualCustomer individualCustomer = new IndividualCustomer();
+        individualCustomer.setId(1);
+        individualCustomer.setPhone("05111111");
+        individualCustomer.setCustomerNumber("12345");
+        individualCustomer.setFirstName("Adem");
+        individualCustomer.setLastName("Ayar");
 
+        CorporateCustomer corporateCustomer = new CorporateCustomer();
+        corporateCustomer.setId(2);
+        corporateCustomer.setPhone("08501111111");
+        corporateCustomer.setCustomerNumber("54862");
+        corporateCustomer.setCompanyName("Java");
+        corporateCustomer.setTaxNumber("1234567890");
 
+        Customer[] customers = {IndividualCustomer,CorporateCustomer};
 
     }
+
 }
